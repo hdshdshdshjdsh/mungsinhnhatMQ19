719f8b6464c1ec18c280b27e9b24cb54ba930d5f
hdshdshdshjdsh <142667244+hdshdshdshjdsh@users.noreply.github.com>
2024-04-18 16:06:23 +0700

    Add files via upload

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mỹ Quỳnh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100011362963430';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = '19-04-2005';
+const contentCard = 'Happy Birthday Mỹ Quỳnh🎂🎉🎊.Chúc Cậu Tuổi Mới Ngày Càng Xinh Đẹp,Học Hành Thật Tốt,Cười Thật Nhiều,Chúc Cậu Tuổi Mới Thành Công Mới.Chúc Cậu Tất Cả Trừ Vất Vả Lo Âu🍀🎊';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'myquynh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
